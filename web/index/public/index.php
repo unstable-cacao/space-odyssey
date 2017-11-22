9f4200af24c9e0a604ba7e169ddfbec96a90837e
Alexey Puchkov <alexey@oktopost.com>
2017-11-22 07:15:54 +0200

    Temp change

--- web/index/public/index.php
+++ web/index/public/index.php
@@ -1,2 +1,40 @@
 <?php
-// boot index server
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+
+$klein = new \Klein\Klein();
+
+
+$klein->respond(function ($request)
+{
+	return UserController::get($request);
+    return 'All the things';
+});
+
+
+$klein->dispatch();
+
+
+class Controller
+{
+	public function __construct(Klein\Request $request)
+	{
+		
+	}
+	
+	
+	public static function __callStatic($name, $arguments)
+	{
+		$self = new static($arguments[0]);
+		$self->$name(...$arguments);
+	}
+}
+
+
+/**
+ * @method static get(...$a): int
+ */
+class UserController
+{
+	
+}
