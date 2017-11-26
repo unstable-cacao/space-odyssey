e2771ac559725198ad4e2e6b5a6ddac31a46a21d
Marina Sherstuk <marinas@commpeak.com>
2017-11-26 16:33:13 +0200

    Mapped all DAOs in config

--- server/config/skeleton/SpaceOdyssey/Base/DAO.php
+++ server/config/skeleton/SpaceOdyssey/Base/DAO.php
@@ -4,3 +4,9 @@
 
 $this->set(\SpaceOdyssey\Base\DAO\IUserDAO::class, \SpaceOdyssey\DAO\UserDAO::class);
 $this->set(\SpaceOdyssey\Base\DAO\ISessionDAO::class, \SpaceOdyssey\DAO\SessionDAO::class);
+$this->set(\SpaceOdyssey\Base\DAO\ILogDAO::class, \SpaceOdyssey\DAO\LogDAO::class);
+$this->set(\SpaceOdyssey\Base\DAO\IPartyDAO::class, \SpaceOdyssey\DAO\PartyDAO::class);
+$this->set(\SpaceOdyssey\Base\DAO\IPartyMemberDAO::class, \SpaceOdyssey\DAO\PartyMemberDAO::class);
+$this->set(\SpaceOdyssey\Base\DAO\IPersonalityDAO::class, \SpaceOdyssey\DAO\PersonalityDAO::class);
+$this->set(\SpaceOdyssey\Base\DAO\IPersonalTraitDAO::class, \SpaceOdyssey\DAO\PersonalTraitDAO::class);
+$this->set(\SpaceOdyssey\Base\DAO\ISkillDAO::class, \SpaceOdyssey\DAO\SkillDAO::class);
